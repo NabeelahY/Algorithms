58b6e524b4e607d9cc9f53594f9dcab90773c4c4
Nabeelah <noblenab13@gmail.com>
2019-10-10 11:39:16 +0100

    Improve on stock prices solution

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,17 +4,24 @@ import argparse
 
 
 def find_max_profit(prices):
+    # current minimum price at hand will be the first value of prices
     current_min_price_so_far = prices[0]
-    max_profit_so_far = 0
+    # max profit will be next value in prices - current min price
+    max_profit_so_far = prices[1] - current_min_price_so_far
 
-    for i in range(len(prices)):
+    # Loop through other values in prices list to compare
+    for i in range(1, len(prices) - 1):
+        # Calculate the next profit by subtracting the previous from the current value
+        new_profit = prices[i] - current_min_price_so_far
+        # Compare new profit with previous profit
+        if new_profit > max_profit_so_far:
+            # if greater than the profit, replace it with the larger profit
+            max_profit_so_far = new_profit
+        # if previous min is greater current value, replace it with the current value
         if current_min_price_so_far > prices[i]:
             current_min_price_so_far = prices[i]
-        if max_profit_so_far < prices[i]:
-            max_profit_so_far = prices[i]
 
-    result = max_profit_so_far - current_min_price_so_far
-    return result
+    return max_profit_so_far
 
 
 if __name__ == '__main__':
